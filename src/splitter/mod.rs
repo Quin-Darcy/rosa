21dcddb6091e6cd770735b6d54c2dcaabf0b6ad7
quin <quin@atsec.com>
2025-05-15 10:27:27 -0500

    Add testing and improve error handling

--- src/splitter/mod.rs
+++ src/splitter/mod.rs
@@ -10,6 +10,8 @@ use std::path::PathBuf;
 use crate::error::SplitterError;
 
 /// Main struct for processing binary files
+#[derive(Debug)]
+#[derive(PartialEq)]
 pub struct RoSplitter {
     file_path: PathBuf,
     num_ros: usize,
@@ -24,12 +26,9 @@ impl RoSplitter {
     /// Will return `Err` if `file` doesn't exist or invalid
     /// number of ROs are given.
     pub fn new(
-        file: impl Into<PathBuf>, 
+        file_path: PathBuf, 
         ros: usize
     ) -> Result<Self, SplitterError> {
-
-        // Convert given argument into PathBuf type
-        let file_path = file.into();
 
         // Check if file exists
         if !file_path.is_file() {
@@ -42,22 +41,47 @@ impl RoSplitter {
             Err(_) => return Err(SplitterError::MetadataError),
         };
 
-        // Check size is divisible by ROs
-        if bits % ros as u64 != 0 {
-            return Err(SplitterError::InvalidSizeError);
-        }
-
         // Check if valid RO number given
         if ros == 0 {
             return Err(SplitterError::InvalidRoError);
         }
 
-        let spliter = RoSplitter {
+        // Check size is divisible by ROs
+        if bits % (ros as u64) != 0 {
+            return Err(SplitterError::InvalidSizeError);
+        }
+
+        let splitter = RoSplitter {
             file_path,
             size: bits,
             num_ros: ros
         };
 
         Ok(splitter)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+
+    #[test]
+    fn test_splitter_new() {
+        let exist_file_name = ".test_file.txt";
+        let non_exist_file_name = "asdflakjs.sdf";
+        let valid_ros = 20;
+        let invalid_ros = 0;
+
+        // Create the absolute paths
+        let exist_file = env::current_dir().expect("failed").join(exist_file_name);
+        let non_exist_file = env::current_dir().expect("failed").join(non_exist_file_name);
+
+        println!("Exist file: {:?}", exist_file);
+        println!("NOn exist file: {:?}", non_exist_file);
+
+        assert_eq!(RoSplitter::new(non_exist_file, valid_ros), Err(SplitterError::NotFileError)); 
+        assert_eq!(RoSplitter::new(exist_file.clone(), valid_ros), Err(SplitterError::InvalidSizeError));
+        assert_eq!(RoSplitter::new(exist_file, invalid_ros), Err(SplitterError::InvalidRoError));
     }
 }
